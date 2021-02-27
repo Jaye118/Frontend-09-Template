8eefde1da821a39507f78457eccadf0809062bcd
Jaye <yueminzhang@natureself.cn>
2021-02-27 21:47:58 +0800

    [week_01] async

---
+++ Week_01/async/for await of.js
@@ -0,0 +1,13 @@
+async function* counter () {
+  let i = 0;
+  while(true) {
+    await sleep(1000);
+    yield i++;
+  }
+}
+
+(async function () {
+  for  await(let v of counter()) {
+    console.log(v);
+  }
+})()
